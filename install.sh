31119db47c2d7e561c3fa7a472612fed332a278d
hairspring-dev <dev@hairspring.local>
2026-09-13 16:07:34 +0000

    install.sh: ship hs-promote

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ BINS="hs-repl hs-log-cli \
 hs-plugin-answer hs-plugin-answersubmit hs-plugin-selfcheck hs-plugin-critic \
 hs-plugin-fileread hs-plugin-reposearch hs-plugin-repoexec \
 hs-plugin-editapply hs-plugin-notescratch hs-plugin-termexec \
-hs-plugin-swarm hs-plugin-policy hs-plugin-scripted hs-plugin-deepseek hs-plugin-provmodel"
+hs-plugin-swarm hs-plugin-policy hs-plugin-scripted hs-plugin-deepseek hs-plugin-provmodel hs-promote"
 
 mkdir -p "$PREFIX/bin" "$BINLINK_DIR" "$CONFIG_DIR"
 for b in $BINS; do
